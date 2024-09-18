94d58359849e56cc75811a3e1b7ed78f5fdeac2b
eva <arnavshukla6500@gmail.com>
2024-09-18 18:47:18 +0530

    buntime v0.7

--- src/components/sample/sample.tsx
+++ src/components/sample/sample.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
     return (
-        <div class="mx-6 mt-[60vh] md:mt-80 lg:mt-60 flex flex-col gap-4 justify-center items-center">  
+        <div class="mx-6 mt-[800px] md:mt-80 lg:mt-60 flex flex-col gap-4 justify-center items-center">  
             <div class="text-[1.75em] md:text-[2.5em] outf">
                 Excel Sheets Telemetry Example
             </div>

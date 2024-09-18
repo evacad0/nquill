c4e95129d927060235c0f936ff4c82ed9b353415
eva <arnavshukla6500@gmail.com>
2024-09-18 19:48:26 +0530

    buntime v0.8

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import Navbar from "~/components/navbar/navbar";
-import { LuInfo } from "@qwikest/icons/lucide";
+import { LuBadgeCheck, LuCheckCheck, LuCheckCircle, LuCheckCircle2, LuInfo } from "@qwikest/icons/lucide";
 import { Link } from "@builder.io/qwik-city";
 import Reel from "~/components/reel/reel";
 import Models from "~/components/models/models";
@@ -28,11 +28,13 @@ export const sendCreds = routeAction$(async (date) => {
   .select()
         
   console.log([data, error]);
+  // return data;
 })
 
 export default component$(() => {
   const action = sendCreds();
-
+  const check = useSignal(false);
+  const frm = useSignal<HTMLFormElement>();
 
   return (
     <main class="scroll-smooth">
@@ -72,26 +74,37 @@ export default component$(() => {
 
       <div id="cta" class="mx-5 xl:mx-96 mt-20 md:mt-40 || shadow-lg flex flex-col items-left p-10 rounded-2xl border-2 border-black border-dashed">
         <div class="flex flex-row items-center outf text-[1.75em] md:text-[2.5em] mb-10">
-            Getting in touch
-            <span class="flex-grow"></span>
-            <span class="text-[1em]">
-                <LuMailPlus />
-            </span>
+          Getting in touch
+          <span class="flex-grow"></span>
+          <span class={"text-[1em] " + (check.value ? "hidden" : "block")}>
+              <LuMailPlus />
+          </span>
+          <span class={"text-[1em] " + (check.value ? "block" : "hidden")}>
+              <LuCheckCircle />
+          </span>
         </div>
-        <Form action={action} class="text-[1.25em] flex flex-col items-start justify-center gap-8">
-                <div>
-                    <label class="intern" for="name">Name</label><br/>
-                    <input required class="grot outline-none border-b-2 border-b-[#0f62fe] focus:border-b-indigo-600" placeholder="John Doe" id="name" name="name" type="text" />
-                </div>
-                <div>
-                    <label class="intern" for="">Telephone</label><br/>
-                    <input required class="grot outline-none border-b-2 border-b-[#0f62fe] focus:border-b-indigo-600" placeholder="+91" id="phone" name="phone" type="tel" />
-                </div>
-                <div>
-                    <label class="intern" for="">Query</label><br/>
-                    <input required class="xl:w-[200%] grot outline-none border-b-2 border-b-[#0f62fe] focus:border-b-indigo-600" placeholder="AI Pipelines" id="q" name="q" type="text" />
-                </div>
-                <button type="submit" class="text-white outf px-4 py-2 rounded-xl bg-[#0f62fe] shadow-md">Let's Talk</button>
+        <Form id="cta-form" action={action} ref={frm} class="text-[1.25em] flex flex-col items-start justify-center gap-8">
+          <div>
+              <label class="intern" for="name">Name</label><br/>
+              <input required class="grot outline-none border-b-2 border-b-[#0f62fe] focus:border-b-indigo-600" placeholder="John Doe" id="name" name="name" type="text" />
+          </div>
+          <div>
+              <label class="intern" for="">Telephone</label><br/>
+              <input required class="grot outline-none border-b-2 border-b-[#0f62fe] focus:border-b-indigo-600" placeholder="+91" id="phone" name="phone" type="tel" />
+          </div>
+          <div>
+              <label class="intern" for="">Query</label><br/>
+              <input required class="xl:w-[200%] grot outline-none border-b-2 border-b-[#0f62fe] focus:border-b-indigo-600" placeholder="AI Pipelines" id="q" name="q" type="text" />
+          </div>
+          <button onClick$={() => {
+              check.value = true
+              frm.value?.reset()
+            }} type="submit" disabled={check.value} class={"outf px-4 py-2 rounded-xl shadow-md" + (check.value ? " shadow-none text-black bg-none border-2 border-[#0f62fe]" : " text-white bg-[#0f62fe]")}>
+            {check.value ? "Submitted" : "Let's Talk"}
+          </button>
+          <div class={"outf " + (check.value ? "block" : "hidden")}>
+            An authorised representative will contact you shortly.
+          </div>
         </Form>
       </div>
 
